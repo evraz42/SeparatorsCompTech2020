4aaec3f695b9fbb49c2a6bba7e6338434ab771a5
sirkz <golang.freelance@gmail.com>
2020-02-01 22:59:00 +0700

    Fix column names for Devices struct

--- backend/api/models/devices.go
+++ backend/api/models/devices.go
@@ -1,9 +1,11 @@
 package models
 
+import "github.com/google/uuid"
+
 type Devices struct {
-	ID           int    `json:"id_device" gorm:"id_device"`
-	Name         string `json:"name_device" gorm:"name_device"`
-	NumberDevice int    `json:"number_device" `
+	ID           uuid.UUID `json:"id_device" gorm:"column:id_device"`
+	Name         string    `json:"name_device" gorm:"column:name_device"`
+	NumberDevice int       `json:"number_device"`
 }
 
 func (db *DB) GetDevices() ([]Devices, error) {
