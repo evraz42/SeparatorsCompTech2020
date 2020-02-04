7a3c64a5ea709a4cbe304184cb1877654b624b6e
lazy_penguin <kind.lazy.penguin@gmail.com>
2020-02-04 09:58:30 +0700

    add realizations for ml-service

---
+++ Source/WebService/WebService/DataTypesInterfaces/Flag.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseControllerCore
+{
+    public partial class Flag
+    {
+        public int Id { get; set; }
+
+        public Guid IdDevice { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public short TypeFlag { get; set; }
+
+        public float[] Positions { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public int? CurrentPosition { get; set; }
+
+        public float CurrentProbability { get; set; }
+
+        public virtual Device IdDeviceNavigation { get; set; }
+
+        public bool IsValid()
+        {
+            return CurrentPosition != null;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Flag flagObj))
+            {
+                return false;
+            }
+
+            if (!flagObj.IdDevice.Equals(IdDevice))
+            {
+                return false;
+            }
+            if (flagObj.TypeFlag != TypeFlag)
+            {
+                return false;
+            }
+            if (flagObj.CurrentPosition != CurrentPosition)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
